b043ba1a0429c67c94926c8cf659a859d4c91834
felipe <felipesscreator@gmail.com>
2022-09-18 14:38:47 -0300

    feat: transfer area

--- src/components/DuoMatch/index.tsx
+++ src/components/DuoMatch/index.tsx
@@ -1,12 +1,22 @@
-import { View, Modal, ModalProps, Text, TouchableOpacity } from 'react-native';
+import { useState } from "react";
+import {
+  View,
+  Modal,
+  ModalProps,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
+import * as Clipboard from "expo-clipboard";
 
-import { MaterialIcons } from '@expo/vector-icons';
-import { CheckCircle } from 'phosphor-react-native';
+import { MaterialIcons } from "@expo/vector-icons";
+import { CheckCircle } from "phosphor-react-native";
 
-import { styles } from './styles';
-import { THEME } from '../../theme';
+import { styles } from "./styles";
+import { THEME } from "../../theme";
 
-import { Heading } from '../Heading';
+import { Heading } from "../Heading";
 
 interface Props extends ModalProps {
   discord: string;
@@ -14,50 +24,54 @@ interface Props extends ModalProps {
 }
 
 export function DuoMatch({ discord, onClose, ...props }: Props) {
+  const [isCopping, setIsCopping] = useState(false);
+
+  async function handleCopyDiscordToClipboard() {
+    setIsCopping(true);
+    await Clipboard.setStringAsync(discord);
+
+    Alert.alert(
+      "Discord Copiado!",
+      "Usuário copiado para a área de transferência"
+    );
+    setIsCopping(false);
+  }
+
   return (
-    <Modal
-      animationType='fade'
-      transparent
-      statusBarTranslucent
-      {...props}
-    >
+    <Modal animationType="fade" transparent statusBarTranslucent {...props}>
       <View style={styles.container}>
         <View style={styles.content}>
-          <TouchableOpacity
-            style={styles.closeIcon}
-            onPress={onClose}
-          >
-            <MaterialIcons 
+          <TouchableOpacity style={styles.closeIcon} onPress={onClose}>
+            <MaterialIcons
               name="close"
               size={20}
               color={THEME.COLORS.CAPTION_500}
             />
           </TouchableOpacity>
 
-          <CheckCircle 
-            size={64}
-            color={THEME.COLORS.SUCCESS}
-            weight="bold"
-          />
+          <CheckCircle size={64} color={THEME.COLORS.SUCCESS} weight="bold" />
 
-          <Heading 
+          <Heading
             title="Let's play!"
             subtitle="Agora é só começar a jogar!"
-            style={{ alignItems: 'center', marginTop: 24 }}
+            style={{ alignItems: "center", marginTop: 24 }}
           />
 
-          <Text style={styles.label}>
-            Adicione seu discord
-          </Text>
+          <Text style={styles.label}>Adicione seu discord</Text>
 
           <TouchableOpacity
             style={styles.discordButton}
+            onPress={handleCopyDiscordToClipboard}
+            disabled={isCopping}
           >
-            <Text style={styles.discord}> 
-              {discord}
+            <Text style={styles.discord}>
+              {isCopping ? (
+                <ActivityIndicator color={THEME.COLORS.PRIMARY} />
+              ) : (
+                discord
+              )}
             </Text>
           </TouchableOpacity>
-
         </View>
       </View>
     </Modal>
